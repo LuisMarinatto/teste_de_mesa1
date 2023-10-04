ce3417a83fc4bb5aec1a36811690a49ff6c5ccec
Marinatto <fernandomarinatto@gmail.com>
2023-10-04 13:58:11 -0300

    ex_1.2

---
+++ TesteDeMesa1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+{
+    int a;
+    int[] v = new int[6];
+    a = 2;
+    while(a < 6)
+    {
+        v[a] = 10 * a;
+        a += 1;
+        Console.WriteLine($"o resultado é: {a}");
+    }
+}
