04dc9a6f6e990f9f95ef4cabd00522ec1a134fb1
Marinatto <fernandomarinatto@gmail.com>
2023-10-05 08:33:09 -0300

    TesteDeMesa1_2

---
+++ TesteDeMesa1_2/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+{
+    int a = 7;
+    int b = a - 6;
+    int[] v = new int[6];
+    while (b < a)
+    {
+        v[b] = b + a;
+        b = b + 2;
+        Console.WriteLine($"resultado: {b}");
+    }
+}
